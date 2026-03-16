57d6755d238fae6a24e5ee8e0cafbe6730559612
Fabian2611 <fabian.schwarz2010@gmail.com>
2026-03-16 16:20:27 +0100

    v1.2.2 some new brews

---
+++ src/main/java/io/fabianbuthere/rpngameplay/event/SafeLocksTab.java
@@ -0,0 +1,19 @@
+package io.fabianbuthere.rpngameplay.event;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.RegistryObject;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class SafeLocksTab {
+    @SubscribeEvent
+    public static void buildTab(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == melonslise.locks.common.init.LocksItems.TAB.getKey()) {
+            for (RegistryObject<Item> entry : melonslise.locks.common.init.LocksItems.ITEMS.getEntries()) {
+                entry.ifPresent(event::accept);
+            }
+        }
+    }
+}
